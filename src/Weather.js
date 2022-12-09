2a670c2ed40a598a526524b1378bc8527ae0ae1c
Vikkkaa <107106845+Vikkkaa@users.noreply.github.com>
2022-12-09 14:41:12 +0000

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,16 @@
 import React from "react";
+import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
+
+function handleSubmit(response){
+    alert(`the weather in ${response.data.name} is ${response.data.main.temp}`);
+}
+
+let apiKey = "c4c34c2ee0b71307b00dc7655493ef9a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleSubmit);
+
   return <h2>Hello Weather</h2>;
 }
