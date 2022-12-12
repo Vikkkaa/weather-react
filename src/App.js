361092d576a6b39a8a21ffa50c74107ca1c39f43
Vikkkaa <107106845+Vikkkaa@users.noreply.github.com>
2022-12-12 13:33:43 +0000

    added loader spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import Weather from "./Weather";
+
+import { Audio } from 'react-loader-spinner';
 import "./App.css";
 
 function App() {
@@ -7,8 +9,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello world</h1>
-        <Weather city="Kyiv" />
+        <Weather city="Maiorca" />
       </header>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
     </div>
   );
 }
