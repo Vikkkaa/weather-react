2d08911616a9ca360724f80270ddea54eb42987a
Vikkkaa <107106845+Vikkkaa@users.noreply.github.com>
2023-01-04 15:49:29 +0000

    built an apicall for weelkly forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
 import "./Forecast.css";
 
-export default function Forecast() {
+export default function Forecast(props) {
+function handleResponse(response) {
+  console.log(response.data);
+}
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(handleResponse);
+  
   return (
     <div className="WeatherForecast">
       <div className="row">
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import City from "./City";
 import CurrentLocation from "./CurrentLocation";
@@ -7,83 +7,75 @@ import Forecast from "./Forecast";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-
 import "./Weather.css";
-
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.cityInput);
-  const [weatherData,setWeatherData] = useState({ready: false});
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleResponse(response){
+  function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
       ready: true,
-      date: new Date(response.data.dt*1000),
+      coord: response.data.coord,
+      date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
       icon: response.data.weather[0].icon,
       description: response.data.weather[0].description,
       humid: response.data.main.humidity,
       wind: response.data.wind.speed,
       city: response.data.name,
-      country: response.data.sys.country
-    }
-
-    );
+      country: response.data.sys.country,
+    });
   }
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
     Search();
   }
 
-  function updateCity(event){
-    
+  function updateCity(event) {
     setCity(event.target.value);
-    
   }
 
-  function Search(){
-  const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-}
+  function Search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
- if (weatherData.ready) {return (
-   <div>
-     <City citySearch = {weatherData} />
-     <form id="search-form" onSubmit={handleSubmit}>
-       <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-         <div className="input-group mb-2">
-           <input
-             type="text"
-             className="form-control"
-             placeholder="Search"
-             aria-label="Search"
-             aria-describedby="button-addon2"
-             onChange={updateCity}
-           />
-           <button
-             className="btn btn-danger"
-             type="button"
-             onclick="submitSearch(event)"
-           >
-             <i className="fa-solid fa-magnifying-glass"></i>
-           </button>
-         </div>
-         <CurrentLocation />
-         
-       </div>
-     </form>
-     <Currentday data={weatherData} />
-     <Forecast />
-   </div>
- );
-}
-  else {
-   Search();
+  if (weatherData.ready) {
     return (
-      "Loading.."
+      <div>
+        <City citySearch={weatherData} />
+        <form id="search-form" onSubmit={handleSubmit}>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+            <div className="input-group mb-2">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+                onChange={updateCity}
+              />
+              <button
+                className="btn btn-danger"
+                type="button"
+                onclick="submitSearch(event)"
+              >
+                <i className="fa-solid fa-magnifying-glass-location"></i>
+              </button>
+            </div>
+            <CurrentLocation />
+          </div>
+        </form>
+        <Currentday data={weatherData} />
+        <Forecast coordinates={weatherData.coord} />
+      </div>
     );
+  } else {
+    Search();
+    return "Loading..";
   }
 }
